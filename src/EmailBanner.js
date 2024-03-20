40583dfe9276d3c7a8bdfaae6c375f5ec2e33000
Shubham Chauhan <chauhanshubham19765@gmail.com>
2024-03-20 09:48:28 +0530

    Year issue in intern project solved

--- src/EmailBanner.js
+++ src/EmailBanner.js
@@ -68,7 +68,7 @@ export function EmailBanner() {
               <a
                 style={{ color: "white", textDecoration: "none" }}
                 target="_blank"
-                href="https://res.cloudinary.com/dcmpkhero/image/upload/v1710851421/projects/my%20resume/Shubham_Chauhan_Resume.pdf"
+                href="https://res.cloudinary.com/dcmpkhero/image/upload/v1710866060/projects/my%20resume/Shubham_Chauhan_Resume.pdf"
                 rel="noreferrer"
               >
                 <Button radius="xl" size="md" className={classes.control}>
